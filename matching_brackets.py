e3a51aa8e66b3ce90a6adb6c8fd4c880ef6c83a2
Jermaine Simon Davies <jermainedavies@outlook.com>
2021-01-24 23:54:26 +0000

    Add files via upload

---
+++ matching_brackets.py
@@ -0,0 +1,27 @@
+def is_valid(s):
+    if len(s) % 2 != 0:
+        return False
+    openers = ["(","{","["]
+    closers = [")","}","]"]
+    bracket_stack = []
+
+    for i in range(len(s)):
+        if s[i] in openers:
+            bracket_stack.append(s[i])
+        elif s[i] in closers:
+            bracket_stack.append(s[i])
+            if closers.index(bracket_stack[-1]) == openers.index(bracket_stack[-2]):
+                for i in range(2):
+                    bracket_stack.pop()
+
+    print(bracket_stack)
+
+    if bracket_stack == []:
+        return True
+    return False
+
+
+
+print(is_valid("()[]{}")) #True
+print(is_valid("(]")) #False
+print(is_valid("{[]}")) # True
