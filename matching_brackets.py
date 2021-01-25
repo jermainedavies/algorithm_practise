272534700adc6fd39825e5c8d41bf88d2170447c
Jermaine Simon Davies <jermainedavies@outlook.com>
2021-01-25 21:29:50 +0000

    accommodates wider range of test cases

--- matching_brackets.py
+++ matching_brackets.py
@@ -1,19 +1,25 @@
 def is_valid(s):
-    if len(s) % 2 != 0:
-        return False
     openers = ["(","{","["]
     closers = [")","}","]"]
     bracket_stack = []
+
+    if len(s) % 2 != 0:
+        return False
+    if s[0] in closers:
+        return False
 
     for i in range(len(s)):
         if s[i] in openers:
             bracket_stack.append(s[i])
         elif s[i] in closers:
             bracket_stack.append(s[i])
+            if bracket_stack[0] in closers:
+                return False
             if closers.index(bracket_stack[-1]) == openers.index(bracket_stack[-2]):
                 for i in range(2):
                     bracket_stack.pop()
-
+            else:
+                return False
 
     if bracket_stack == []:
         return True
@@ -24,3 +30,5 @@ def is_valid(s):
 print(is_valid("()[]{}")) #True
 print(is_valid("(]")) #False
 print(is_valid("{[]}")) # True
+print(is_valid("([)]")) # False
+print(is_valid("(){}}{")) # False
